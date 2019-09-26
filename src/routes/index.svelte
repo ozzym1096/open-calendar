b826f93df2c3cf713132b5b8f5bca9e50a7d0354
Ozmar Mendoza <ozmarmen@gmail.com>
2019-09-26 09:06:58 -0500

    Removed unused CSS selectors

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,38 +12,6 @@ import monthsArr from '../utils/monthsArr.js';
 	<Month month={item}/>
 </VirtualList>
 
-<style global>
-	h1,
-	figure,
-	p {
-		margin: 0 auto;
-		text-align: center;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-		text-transform: uppercase;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		margin: 0 0 1em 0;
-		max-width: 400px;
-		width: 100%;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+<style>
+	
 </style>
