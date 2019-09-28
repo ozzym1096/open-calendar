323db7f97a7742e11d51e3daf85ea29e4d27a1e9
Ozmar Mendoza <ozmarmen@gmail.com>
2019-09-28 09:32:05 -0500

    Make virtual list rows overlap

--- src/components/Month.svelte
+++ src/components/Month.svelte
@@ -1,24 +1,51 @@
 <script>
 export let month;
+// @TODO margin-bottom for months ending on a Saturday
+const firstDayOffset = new Date(month.year, month.month, 1).getDay();
+const firstMonthTest = month.year !== 2019 || month.month !== 0;
+
 const daysArr = [];
 for (let i = 1; i <= month.days; i++) {
 	daysArr.push(i)
 }
 </script>
 
-<div class="container">
-	{#each daysArr as day}
-		<div class="day"><p>{month.year},{month.month}: {day} days</p></div>
+<div class="month" class:not-first-month={firstMonthTest}>
+	{#each daysArr as day, i}
+		{#if i === 0}
+			<div class="day first-day" style={`margin-left: calc(var(--day-width) * ${firstDayOffset})`}><p>{month.month + 1}.{day}.{month.year}</p></div>
+		{:else}
+			<div class="day"><p>{month.month + 1}.{day}.{month.year}</p></div>
+		{/if}
 	{/each}
 </div>
 
-<style>
-	.container {
-		display: flex;
-		flex-direction: row;
-		flex-wrap: wrap;
+<style lang="scss">
+	* {
+		--day-width: calc(100% / 7);
 	}
-	.day{
+
+	p {
+		font-size: 0.75rem;
+	}
+
+	.month {
+		display: flex;
+		flex-flow: row wrap;
 		width: 100%;
+
+		&.not-first-month {
+			margin-top: -5vh;
+		}
+	}
+
+	.day {
+		flex-basis: var(--day-width);
+		height: 5vh;
+
+		&:first-child {
+			border: solid 1px red;
+			box-sizing: border-box;
+		}
 	}
 </style>
