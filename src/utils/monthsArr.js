fdf5c6ca6f885264a886b963e2e7590264995bd3
Ozmar Mendoza <ozmarmen@gmail.com>
2019-09-28 10:56:01 -0500

    Use one date to get all info to push to monthsArr

--- src/utils/monthsArr.js
+++ src/utils/monthsArr.js
@@ -2,49 +2,31 @@ const minYear = 2019;
 const maxYear = 2060;
 
 function createMonthsArr(minYear, maxYear) {
-	const months = [];
-    
-	function pushToMonthsArr(year, month, days) {
-		months.push({
-			year,
-			month,
-			days
-		})
-	}
+	let months = [];
+	let lastMonthDay = Date.now()
 
 	// Loop over years
 	for (let currentY = minYear; currentY <= maxYear; currentY++) {
 		// Loop over months per year
 		// 0-based index for months
 		for (let currentM = 0; currentM <= 11; currentM++) {
-			switch (currentM) {
-			case 0:
-			case 2:
-			case 4:
-			case 6:
-			case 7:
-			case 9:
-			case 11:
-				pushToMonthsArr(currentY, currentM, 31)
-				break;
-			case 3:
-			case 5:
-			case 8:
-			case 10:
-				pushToMonthsArr(currentY, currentM, 30)
-				break;
-			case 1:
-				pushToMonthsArr(currentY, currentM, new Date(currentY, currentM + 1, 0).getDate())
-				break;
-			}
+			lastMonthDay = new Date(currentY, currentM + 1, 0);
+		      	months.push({
+				year: lastMonthDay.getFullYear(),
+				month: lastMonthDay.getMonth(),
+				days: lastMonthDay.getDate(),
+				// Used to decide whether or not to add padding-bottom to months that end on a Saturday
+				endsOnSat: lastMonthDay.getDay() === 6
+			})
 		}
 	}
 	/*
 		[
 			{
-				Year: YYYY
-				Month: D;
-				Days: DD
+				year: YYYY,
+				month: DD,
+				days: DD,
+				endsOnSat: bool
 			}
 			...
 		]
