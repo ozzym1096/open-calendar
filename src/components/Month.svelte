9858edfd35cf789529aebcd024d1800a25b6f907
Ozmar Mendoza <ozmarmen@gmail.com>
2019-09-30 13:06:47 -0500

    Each list item ends on Saturday & starts on Sunday

--- src/components/Month.svelte
+++ src/components/Month.svelte
@@ -1,22 +1,18 @@
 <script>
 export let month;
-// @TODO margin-bottom for months ending on a Saturday
-const firstDayOffset = new Date(month.year, month.month, 1).getDay();
-const firstMonthTest = month.year !== 2019 || month.month !== 0;
 
-const daysArr = [];
-for (let i = 1; i <= month.days; i++) {
-	daysArr.push(i)
+const lastDateBool = month.lastDate > month.lastDateOfMonth
+
+let datesArr = [];
+
+for (let i = month.startDate; i <= month.lastDate; i++) {
+	datesArr.push(new Date(month.year, month.month, i))
 }
 </script>
 
-<div class="month" class:not-first-month={firstMonthTest} class:ends-on-sat={month.endsOnSat}>
-	{#each daysArr as day, i}
-		{#if i === 0}
-			<div class="day first-day" style={`margin-left: calc(var(--day-width) * ${firstDayOffset})`}><p>{month.month + 1}.{day}.{month.year}</p></div>
-		{:else}
-			<div class="day"><p>{month.month + 1}.{day}.{month.year}</p></div>
-		{/if}
+<div class="month">
+	{#each datesArr as date}
+		<div class="day" class:first-day={date.getDate() === 1}><p>{date.getMonth() + 1}.{date.getDate()}.{date.getFullYear()}</p></div>
 	{/each}
 </div>
 
@@ -33,23 +29,15 @@ for (let i = 1; i <= month.days; i++) {
 		display: flex;
 		flex-flow: row wrap;
 		width: 100%;
-
-		&.not-first-month {
-			margin-top: -5vh;
-		}
-
-		&.ends-on-sat {
-			padding-bottom: 5vh;
-		}
 	}
 
 	.day {
 		flex-basis: var(--day-width);
 		height: 5vh;
+	}
 
-		&:first-child {
-			border: solid 1px red;
-			box-sizing: border-box;
-		}
+	.first-day {
+		border: solid 1px red;
+		box-sizing: border-box;
 	}
 </style>
--- src/utils/monthsArr.js
+++ src/utils/monthsArr.js
@@ -2,31 +2,42 @@ const minYear = 2019;
 const maxYear = 2060;
 
 function createMonthsArr(minYear, maxYear) {
+	let firstDateOfMonth = Date.now();
+	let lastDateOfMonth = Date.now()
+	let index = 1;
 	const months = [];
-	let lastMonthDay = Date.now()
 
 	// Loop over years
 	for (let currentY = minYear; currentY <= maxYear; currentY++) {
 		// Loop over months per year
 		// 0-based index for months
 		for (let currentM = 0; currentM <= 11; currentM++) {
-			lastMonthDay = new Date(currentY, currentM + 1, 0);
+			firstDateOfMonth = new Date(currentY, currentM, 1);
+			lastDateOfMonth = new Date(currentY, currentM + 1, 0);
+
 			months.push({
-				year: lastMonthDay.getFullYear(),
-				month: lastMonthDay.getMonth(),
-				days: lastMonthDay.getDate(),
-				// Used to decide whether or not to add padding-bottom to months that end on a Saturday
-				endsOnSat: lastMonthDay.getDay() === 6
+				_index: index,
+				year: firstDateOfMonth.getFullYear(),
+				month: firstDateOfMonth.getMonth(),
+				startDate:
+					currentY === minYear && currentM === 0 ? 1 : firstDateOfMonth.getDay() === 0 ? 1 : firstDateOfMonth.getDate() + (7 - firstDateOfMonth.getDay()),
+				lastDateOfMonth: lastDateOfMonth.getDate(),
+				lastDate:
+					currentY === maxYear && currentM === 11 ? 31 : lastDateOfMonth.getDay() === 6 ? lastDateOfMonth.getDate() : lastDateOfMonth.getDate() + (6 - lastDateOfMonth.getDay()),
 			})
+
+			index++
 		}
 	}
 	/*
 		[
 			{
-				year: YYYY,
-				month: DD,
-				days: DD,
-				endsOnSat: bool
+				_index: int
+				year: int,
+				month: int,
+				startDate: int,
+				lastDateOfMonth: int,
+        		lastDate: int
 			}
 			...
 		]
