01d1b67c5b1289bfecabc7d4ef65a02ec3cd7365
Ozmar Mendoza <ozmarmen@gmail.com>
2019-10-01 13:19:53 -0500

    Check when virtual list scrolls to bottom/top

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,8 +2,21 @@
 import VirtualList from '@sveltejs/svelte-virtual-list/VirtualList.svelte';
 import Month from '../components/Month.svelte'
 import monthsArr from '../utils/monthsArr.js';
+import {onMount} from 'svelte';
 
 let start, end;
+
+function checkScrollEnd(e) {
+	let elem = e.currentTarget;
+	if (elem.scrollHeight - elem.scrollTop === elem.offsetHeight) {
+		console.log("bottom");
+	}
+
+}
+
+onMount(() => {
+	document.querySelector('svelte-virtual-list-viewport').addEventListener('scroll', checkScrollEnd);
+})
 </script>
 
 <svelte:head>
@@ -11,7 +24,7 @@ let start, end;
 </svelte:head>
 
 <div class="calendar">
-	<VirtualList itemHeight={175} items={monthsArr} let:item bind:start bind:end>
+	<VirtualList itemHeight={235} items={monthsArr} let:item bind:start bind:end>
 		<Month month={item}/>
 	</VirtualList>
 
