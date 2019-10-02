8eff99dbab7d7c9205fc695cdb21094b009538c2
Ozmar Mendoza <ozmarmen@gmail.com>
2019-10-02 12:49:42 -0500

    Created functions to modify monthsArr

--- src/utils/monthsArr.js
+++ src/utils/monthsArr.js
@@ -1,5 +1,4 @@
-const minYear = 2019;
-const maxYear = 2020;
+'use strict'
 
 function createMonthsArr(minYear, maxYear) {
 	let firstDateOfMonth = Date.now();
@@ -45,4 +44,42 @@ function createMonthsArr(minYear, maxYear) {
 	return months;
 }
 
-export default createMonthsArr(minYear, maxYear);
+function pushToMonthsArr(monthsArr) {
+	const lastItem = monthsArr[monthsArr.length - 1];
+	const minYear = lastItem.year + 1;
+	const itemsToAdd = [];
+
+	let index = lastItem._index + 1;
+
+	let firstDateOfMonth = Date.now();
+	let lastDateOfMonth = Date.now();
+
+	// Loop over years
+	for (let currentY = minYear; currentY <= minYear + 2; currentY++) {
+		// Loop over months per year
+		// 0-based index for months
+		for (let currentM = 0; currentM <= 11; currentM++) {
+			firstDateOfMonth = new Date(currentY, currentM, 1);
+			lastDateOfMonth = new Date(currentY, currentM + 1, 0);
+
+			itemsToAdd.push({
+				_index: index,
+				year: firstDateOfMonth.getFullYear(),
+				month: firstDateOfMonth.getMonth(),
+				startDate:
+					firstDateOfMonth.getDay() === 0 ? 1 : firstDateOfMonth.getDate() + (7 - firstDateOfMonth.getDay()),
+				lastDateOfMonth: lastDateOfMonth.getDate(),
+				lastDate:
+					currentY === minYear + 2 && currentM === 11 ? 31 : lastDateOfMonth.getDay() === 6 ? lastDateOfMonth.getDate() : lastDateOfMonth.getDate() + (6 - lastDateOfMonth.getDay()),
+			})
+			index++
+		}
+	}
+	return [...monthsArr, ...itemsToAdd]
+}
+
+function sliceMonthsArr(monthsArr) {
+	return monthsArr.slice(24)
+}
+
+export {createMonthsArr, pushToMonthsArr, sliceMonthsArr};
